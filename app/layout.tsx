688ddd717fffb9861a4b2d1aa8970aaa37eb487d
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-08-27 20:48:33 +0700

    move globals css to dedicated folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
+import "@/src/ui/css/globals.tailwind.css";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
